1aba2b478e8c4872b87848edd469b5e1fefd134c
LeroyM084 <mleroy@mleroy.fr>
2026-04-22 10:03:11 +0200

    second test version

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -1,10 +1,20 @@
 import { Client } from "@gradio/client";
 
-export function modifyContent(content) {
+export async function modifyContent(content) {
+    const client = createGradioClient();
+    const result = await client.predict("/generate", {
+		message: "Hello!!",
+		max_new_tokens: 1024,
+		temperature: 0.6,
+		top_p: 0.9,
+		top_k: 50,
+		repetition_penalty: 1.2,
+    })
 
+    return result.data;
 }
 
-function createGradioClient() {
+async function createGradioClient() {
     const client = await Client.connect("hysts/mistral-7b");
     return client;
 }
